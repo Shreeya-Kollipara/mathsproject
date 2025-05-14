422ec7e0fe1d015f024def964420a90fa57923a1
Shreeya2805 <kollipara.naga2023@vitstudent.ac.in>
2025-05-14 12:20:27 +0530

    Update for Render deployment

--- app.py
+++ app.py
@@ -684,5 +684,8 @@ def calculate_characteristic_polynomial():
 def index():
     return render_template('index.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
